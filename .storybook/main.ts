c91db33b78ab4d996f93f4beabfe7746616247f5
sangwookpark <sangwookp9591@gmail.com>
2025-08-15 00:24:28 +0900

    refactor : storybook 에서 vanilla-extract제거

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,15 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
-import { VanillaExtractPlugin } from '@vanilla-extract/webpack-plugin';
 
 const config: StorybookConfig = {
-    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-    addons: ['@storybook/addon-webpack5-compiler-swc', '@storybook/addon-docs', '@storybook/addon-onboarding'],
-    framework: {
-        name: '@storybook/react-webpack5',
-        options: {},
-    },
-    webpackFinal: async (config) => {
-        // 기존 플러그인 배열에 Vanilla Extract 플러그인 추가
-        if (config.plugins) {
-            config.plugins.push(new VanillaExtractPlugin());
-        } else {
-            config.plugins = [new VanillaExtractPlugin()];
-        }
-        return config;
-    },
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-webpack5-compiler-swc',
+    '@storybook/addon-docs',
+    '@storybook/addon-onboarding',
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
 };
 export default config;
