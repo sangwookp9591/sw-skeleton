841dabffeb040da50f9c84cbf4ed26a15d71b111
sangwookpark <sangwookp9591@gmail.com>
2025-08-14 13:23:58 +0900

    feat : skeleton stories tsx추가

--- src/Skeleton.stories.tsx
+++ src/Skeleton.stories.tsx
@@ -2,13 +2,49 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Skeleton, { SkeletonProps } from './Skeleton';
 
 const meta: Meta = {
-    title: 'component/skeleton',
+    title: 'components/skeleton',
     component: Skeleton,
     tags: ['autodocs'],
+    argTypes: {
+        width: { control: 'text' },
+        height: { control: 'text' },
+        borderRadius: { control: 'text' },
+        backgroundColor: { control: 'color' },
+    },
 };
 
 export default meta;
 
 type Story = StoryObj<SkeletonProps>;
 
-export const Default: Story = {};
+export const Default: Story = {
+    args: {
+        width: '100px',
+        height: '100px',
+    },
+};
+
+export const Circle: Story = {
+    args: {
+        width: '100px',
+        height: '100px',
+        borderRadius: '50px',
+    },
+};
+export const RedCircle: Story = {
+    args: {
+        width: '100px',
+        height: '100px',
+        borderRadius: '50px',
+        backgroundColor: 'red',
+    },
+};
+
+export const Red: Story = {
+    args: {
+        width: '100px',
+        height: '100px',
+        borderRadius: '50px',
+        backgroundColor: 'blue',
+    },
+};
