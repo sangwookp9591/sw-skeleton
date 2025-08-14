23d4bed67d7137124ee09abccf954bcc21a84fc2
sangwookpark <sangwookp9591@gmail.com>
2025-08-14 13:22:15 +0900

    feat : skeleton 디자인 적용

--- src/Skeleton.tsx
+++ src/Skeleton.tsx
@@ -1,7 +1,24 @@
 import styles from './Skeleton.module.css';
 
-export type SkeletonProps = {};
+export type SkeletonProps = {
+    width: number | string;
+    height: number | string;
+    borderRadius?: number | string;
+    backgroundColor?: string;
+    style?: React.CSSProperties;
+};
 
-export default function Skeleton() {
-    return <div className={styles.skeleton}></div>;
+export default function Skeleton({ width, height, borderRadius, backgroundColor, style }: SkeletonProps) {
+    return (
+        <div
+            className={styles.skeleton}
+            style={{
+                width,
+                height,
+                borderRadius: borderRadius || 0,
+                backgroundColor: backgroundColor || 'rgb(230, 230, 230)',
+                ...style,
+            }}
+        ></div>
+    );
 }
