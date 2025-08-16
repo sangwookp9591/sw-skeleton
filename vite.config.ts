a45113f941c1afbca4cdb983ec670e475628db0f
sangwookpark <sangwookp9591@gmail.com>
2025-08-16 18:55:15 +0900

    chore : vite 로 교체

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'SwSkeleton',
+      formats: ['es', 'umd'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    cssCodeSplit: false,
+    sourcemap: true,
+  },
+});
